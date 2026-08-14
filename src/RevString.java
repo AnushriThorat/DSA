a82dc95e73bbe9c73066a0c46c1255030a040006
Anushri <thoratanushri@gmail.com>
2026-08-14 10:55:40 +0530

    Reverse A string

---
+++ src/RevString.java
@@ -0,0 +1,22 @@
+//Reverse a string
+public class RevString {
+
+    public static void main(String args[]) {
+        String str = "Anushri";
+
+        char[] ch = str.toCharArray();
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (ch[left] != ch[right]) {
+                char temp = ch[left];
+                ch[left] = ch[right];
+                ch[right] = temp;
+            }
+            left++;
+            right--;
+        }
+        System.out.println("Reverse String:" +new String(ch);
+    }
+}
